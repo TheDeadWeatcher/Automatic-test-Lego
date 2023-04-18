afe338adaf97c1b32010d42f35064ce1c4d01e42
Bartosz <jasykbartosz@gmail.com>
2023-04-18 21:47:53 +0200

    hover - trigger comands

---
+++ cypress/e2e/testHover.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+// this test was created for checking only  (hover elements or hidden)
+
+describe("E2E - test for triigger commands", () => {
+ 
+
+  it("Should be correctly log in to Lego account ", () => {
+    cy.visit("https://www.amazon.com/");
+    cy.get(".nav-line-2 ").contains("Account & Lists").trigger('mouseover');
+    cy.get(".nav-text").contains("Create a List").click(); 
+  });
+
+  
+});
+
+
+// working well :) 
